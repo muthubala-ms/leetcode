84a3f5ba640ecb2bea57dd0da2a881bea273cddd
muthubala-ms <muthubala.tech@gmail.com>
2025-03-04 18:34:12 +0530

    feat : solving the 1929.contatarray problem

---
+++ ConcatArray.java
@@ -0,0 +1,22 @@
+/**
+ *  qeustion : 1929. Concatenation of Array
+ */
+
+import java.util.Arrays;
+
+public class ConcatArray{
+
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(getConcatenation(new int[]{1,2,1})));
+    }
+
+    // this method take an array and return new array name as ans with doblue values
+    static public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length*2];
+        for(int i=0;i<nums.length;i++){
+            ans[i] = nums[i];
+            ans[i+nums.length] = nums[i];
+        }
+        return ans;
+    }
+};
